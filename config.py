5df78e873a8510151456ab073edf21ac4435fa2a
noah <noahburrell0@gmail.com>
2018-07-19 17:33:50 -0400

    Tidy config

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 import mysql.connector
 
 # X-Auth-Token.
-auth = \
-    ''
+auth = ''
 
 # ID of Openstack user preforming operations (probably the admin user)
 userID = "a67f74b73fd8478e8181189c9856446f"
@@ -11,13 +10,12 @@ userID = "a67f74b73fd8478e8181189c9856446f"
 userPassword = "labstack"
 
 # TennantID can be found in the OpenStack dashboard under Project -> API Access -> View Credentials -> Project ID
-tennantID = \
-    '9c391e5391684857b4dbd41c93d50b98'
+tennantID = '9c391e5391684857b4dbd41c93d50b98'
 
 # Location of the API. Localhost when running through an SSH tunnel or on the controller. Access to port 80 and 9696 required.
-location = \
-    'localhost'
+location = 'localhost'
 
+# Initialize connection to database
 database = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -25,12 +23,9 @@ database = mysql.connector.connect(
     database="telus"
 )
 
+# Define various database table name
 usertable = "loginInfo"
-
 routertable = "routerTable"
-
 subnettable = "subnetTable"
-
 porttable = "portTable"
-
 devicetable = "deviceTable"
