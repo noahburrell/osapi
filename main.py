863be65e460cfe70668839c44a1084ffaba101ec
noah <noahburrell0@gmail.com>
2018-07-19 17:07:45 -0400

    Delete network functionality and debugging parameters added

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import argparse
 from admin import *
-from postNetwork import *
-from getNetwork import *
-from deleteNetwork import *
-from putNetwork import *
+from getNetwork import getSubnets
 import operations
 import config
 
@@ -14,13 +11,19 @@ parser.add_argument('-n', type=str, help='Create a new subnet with the specified
 parser.add_argument('-N', type=str, help='Optionally used in conjunction with -n to override the network address (use CIDR notation')
 parser.add_argument('-d', type=int, help='Destroy a network with the specified subnet ID')
 
+parser.add_argument('-vv', dest='debugging', action='store_true')
+parser.set_defaults(debugging=False)
+
 parser.add_argument('USER_ID', type=str, help='ID of user the operations will be preformed on')
 
 args = parser.parse_args()
 
-# uid = "1"  # Debugging
-args.n = "test-network"  # Debugging
+# QUICK DEBUG - SET PARSER ARG OVERRIDES HERE
+# args.USER_ID = "1"  # Debugging
+# args.n = "test-network"  # Debugging
 # args.N = "192.168.100.0/24"  # Debugging
+# args.d = "49"  # Debugging
+# args.debugging = False  # Debugging
 
 # SETUP
 # Get X-Auth-Token
@@ -37,10 +40,10 @@ pubnet = result['subnets'][0]['network_id']
 # If the user does not have a vRouter, try to create one. If one cannot be created, raise an error
 # This function will also raise an error if the uid specified cannot be found
 print "Check user has a vRouter:",
-routerID = operations.checkRouter(args.USER_ID)
+routerID = operations.checkRouter(args.USER_ID, args.debugging)
 if routerID is None:
     print "[FALSE]"
-    routerID = operations.createRouter(args.USER_ID, pubnet, pubsub)
+    routerID = operations.createRouter(args.USER_ID, pubnet, pubsub, args.debugging)
     if routerID is None:
         raise ValueError('User does not have a router and one was not able to be created. No further operations can be performed.')
     else:
@@ -51,59 +54,8 @@ print "Router ID is: "+routerID
 
 # Attempt to create a network using the specified name and network address (if applicable)
 if args.n is not None:
-    operations.createNetwork(args.USER_ID, routerID, args.n, args.N)
+    operations.createNetwork(args.USER_ID, routerID, args.n, args.N, args.debugging)
 
 # Attempt to destroy a network using the subnet id (as defined in our database, not the openstack id)
 if args.d is not None:
-    operations.destroyNetwork(args.d)
-
-'''
-# CREATE NETWORK
-# Create Router
-routerResults.append(newRouter("USER-A", pubnet, pubsub, "10.80.1.131"))
-print json.dumps(routerResults, indent=4)
-
-# Create Network
-networkResults.append(newNetwork("USER-A-NET1"))
-networkResults.append(newNetwork("USER-A-NET2"))
-print json.dumps(networkResults, indent=4)
-
-# Create Subnet
-subnetResults.append(newSubnet(networkResults[0]['network']['id'], "192.168.101.0/24"))
-subnetResults.append(newSubnet(networkResults[1]['network']['id'], "192.168.102.0/24"))
-print json.dumps(routerResults, indent=4)
-
-# Add ports to router on subnet
-portResults.append(newRouterPort(routerResults[0]['router']['id'], subnetResults[0]['subnet']['id']))
-portResults.append(newRouterPort(routerResults[0]['router']['id'], subnetResults[1]['subnet']['id']))
-print json.dumps(portResults, indent=4)
-# '''
-
-# raw_input("Press Enter to continue...")
-
-'''
-# DESTROY NETWORK (Reverse order of CREATE)
-# Get X-Auth-Token
-config.auth = getToken("a67f74b73fd8478e8181189c9856446f", "labstack")
-
-# Delete ports on router
-portResults[0] = delPort(routerResults[0]['router']['id'], portResults[0]['port_id'])
-portResults[1] = delPort(routerResults[0]['router']['id'], portResults[1]['port_id'])
-print json.dumps(portResults, indent=4)
-
-# Delete Subnets
-if delSubnet(subnetResults[0]['subnet']['id']):
-    print "Subnet " + subnetResults[0]['subnet']['id'] + " Deleted"
-if delSubnet(subnetResults[1]['subnet']['id']):
-    print "Subnet " + subnetResults[1]['subnet']['id'] + " Deleted"
-
-# Delete Networks
-if delNetwork(networkResults[0]['network']['id']):
-    print "Network " + networkResults[0]['network']['id'] + " Deleted"
-if delNetwork(networkResults[1]['network']['id']):
-    print "Network " + networkResults[1]['network']['id'] + " Deleted"
-
-# Delete Router
-if delRouter(routerResults[0]['router']['id']):
-    print "Router " + routerResults[0]['router']['id'] + " Deleted"
-# '''
+    operations.destroyNetwork(args.USER_ID, args.d, args.debugging)
--- operations.py
+++ operations.py
@@ -8,12 +8,15 @@ import config
 import mysql.connector
 
 
-def checkRouter(uid):
+def checkRouter(uid, debugging=False):
     # Check if the user has a vRouter
     connection = config.database.cursor(dictionary=True)
     connection.execute("SELECT * FROM loginInfo LEFT OUTER JOIN routerTable ON loginInfo.id = routerTable.uid WHERE loginInfo.id = '" + uid + "';")
 
     result = connection.fetchone()
+    if debugging:
+        print "Matched user/vRouter: "+json.dumps(result, indent=4)
+
     if connection.rowcount is not 1:
         raise ValueError('User ID "'+uid+'" does not exist or the database cannot be accessed.')
 
@@ -21,9 +24,9 @@ def checkRouter(uid):
     return result['osrid']
 
 
-def createRouter(uid, pubnet, pubsub):
+def createRouter(uid, pubnet, pubsub, debugging=False):
     # Create a new router for the user
-    result = newRouter(uid, pubnet, pubsub)
+    result = newRouter(uid, pubnet, pubsub, None, debugging)
 
     # Insert a new row into the database
     connection = config.database.cursor()
@@ -39,13 +42,15 @@ def createRouter(uid, pubnet, pubsub):
         return None
 
 
-def createNetwork(uid, osrid, name, network):
+def createNetwork(uid, osrid, name, network, debugging=False):
     # Initiate a new SQL connection
     connection = config.database.cursor(dictionary=True)
 
     # Look up the ID of the router in our DB
     connection.execute("SELECT id FROM routerTable WHERE osrid = '" + osrid + "'")
     rid = connection.fetchone()
+    if debugging:
+        print json.dumps(rid, indent=4)
     if connection.rowcount is not 1:
         raise ValueError('vRouter ID "' + osrid + '" does not exist or the database cannot be accessed.')
     rid = rid['id']
@@ -56,19 +61,27 @@ def createNetwork(uid, osrid, name, network):
     # Get all networks in use by the specified user, joining the subnet table on the router table and looking up the uid specified in the router table
     connection.execute("SELECT cidr FROM subnetTable INNER JOIN routerTable ON subnetTable.rid = routerTable.id WHERE routerTable.uid = '" + uid + "'")
     addressesInUse = connection.fetchall()
+    if debugging:
+        print "Found existing networks for this user: "+json.dumps(addressesInUse, indent=4)
     if network is None:
         # Iterate through 192.168.2.0/24 to 192.168.254.0/24 (Private class A range minus commonly used 192.168.0.0/24 and 192.168.1.0/24)
         for i in range(2, 254):
             # Assume the network being tried is not in use
             inUse = False
+            if debugging:
+                print "Trying: 192.168."+str(i)+".0/24"
             # Try to find the network in the list of used networks (for the specified UID) in the DB
             for address in addressesInUse:
                 # Set inUse to true if the network exists in the DB
                 if address['cidr'] == "192.168."+str(i)+".0/24":
+                    if debugging:
+                        print "Already exists!"
                     inUse = True
                     break
             # If the tried network is still not seen as inUse after iterating through DB results, set network to the tried network for use later
             if inUse is False:
+                if debugging:
+                    print "HIT!"
                 network = "192.168."+str(i)+".0/24"
                 break
         print "Automatically selecting network: " + network
@@ -85,13 +98,13 @@ def createNetwork(uid, osrid, name, network):
         raise ValueError("No networks available!")
 
     # Create a new network
-    netResult = newNetwork(name)
+    netResult = newNetwork(name, debugging)
     if netResult['network']['id'] is None:
         raise ValueError("Failed to create network")
     print "New network ID is: '"+netResult['network']['id']
 
     # Create a subnet and associate it to the created network
-    subnetResult = newSubnet(netResult['network']['id'], network)
+    subnetResult = newSubnet(netResult['network']['id'], network, debugging)
     if subnetResult['subnet']['id'] is not None:
 
         # Insert row into subnet table
@@ -112,9 +125,11 @@ def createNetwork(uid, osrid, name, network):
     if connection.rowcount is not 1:
         raise ValueError('Subnet ID "' + subnetResult['subnet']['id'] + '" does not exist or the database cannot be accessed.')
     subid = subid['id']
+    if debugging:
+        print "Matched to subnet: "+str(subid)
 
     # Create a port and associate it with a router and subnet
-    portResult = newRouterPort(osrid, subnetResult['subnet']['id'])
+    portResult = newRouterPort(osrid, subnetResult['subnet']['id'], debugging)
     if portResult['port_id'] is not None:
         sql = "INSERT INTO portTable (rid, sid, secid, ipaddr, ospid, osdid) VALUES (%s, %s, %s, %s, %s, %s)"
         val = (int(rid), int(subid), "0", getPorts([["id", str(portResult['port_id'])]])['ports'][0]['fixed_ips'][0]['ip_address'], portResult['port_id'], portResult['id'])  # SecID = 0 is temporary until we evaluate the security implementation
@@ -130,6 +145,35 @@ def createNetwork(uid, osrid, name, network):
     return
 
 
-def destroyNetwork(subnetid):
+def destroyNetwork(uid, subnetid, debugging=False):
+    # Initiate a new SQL connection
+    connection = config.database.cursor(dictionary=True)
+
+    # Look up the subnet ID in subnet table and join on the ports table since the port needs to be deleted first (if one exists)
+    # Also inner join the router table to lookup the uid to ensure the subnet being deleted is owned by the specified uid. The uid is just a sanity check
+    connection.execute("SELECT * FROM subnetTable INNER JOIN routerTable ON subnetTable.rid = routerTable.id LEFT OUTER JOIN portTable ON subnetTable.id = portTable.sid WHERE subnetTable.id = '" + subnetid + "' AND routerTable.uid = '"+uid+"'")
+    results = connection.fetchone()
+    if connection.rowcount is 0:
+        raise ValueError('Subnet ID "' + subnetid + '" does not exist or the database cannot be accessed.')
+    if debugging:
+        print "Get result where user ID="+uid+" and subnet ID="+subnetid+": "+json.dumps(results, indent=4)
+
+    # Delete port
+    delPort(results['osrid'], results['ospid'], debugging)
+    connection.execute("DELETE FROM portTable WHERE sid='"+subnetid+"'")
+    if connection.rowcount is 0:
+        raise ValueError('Could not delete port with sid="' + subnetid + '". Does not exist or the database cannot be accessed.')
+    print "Port deleted: "+results['ospid']
+    config.database.commit()
+
+    # Delete subnet and parent network (contained in same DB table)
+    delSubnet(results['ossubid'])
+    delNetwork(results['osnetid'])
+    connection.execute("DELETE FROM subnetTable WHERE id='"+subnetid+"'")
+    if connection.rowcount is 0:
+        raise ValueError('Could not delete subnet with id="' + subnetid + '". Does not exist or the database cannot be accessed.')
+    print "Subnet deleted: "+results['ossubid']
+    print "Network deleted: " + results['osnetid']
+    config.database.commit()
 
     return
